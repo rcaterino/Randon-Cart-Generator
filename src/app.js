a13f171becc9ef01eb44c511e7aaa1dc044b98e5
Ricardo Fabio Caterino Polini <104625061+rcaterino@users.noreply.github.com>
2022-06-24 20:52:16 +0200

    Random card generator

--- src/app.js
+++ src/app.js
@@ -1,41 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = () => {
-  //write your code here
-  const boton = document.querySelector("#boton");
-  // Agregar listener
-  boton.addEventListener("click", function(evento) {
-    // Aquí todo el código que se ejecuta cuando se da click al botón
-    document.querySelector("#excuse").innerHTML = generateExcuse();
-    //alert("Le has dado click");
-  });
+  let suit = getRandomSuit();
+  let cambio = document.getElementById("card");
+  cambio.style.color = getRandomColor();
+  document.querySelector(".number").innerHTML = getRandomNum();
+  document.querySelector(".top").innerHTML = suit;
+  document.querySelector(".botton").innerHTML = suit;
 };
 
-let generateExcuse = () => {
-  let who = ["the dog", "my granma", "his turtle", "my bird"];
-  let what = ["eat", "pissed", "crushed", "broked"];
-  let when = [
-    "before the class",
-    "right in time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-
-  let quienPosition = Math.floor(Math.random() * 4);
-  let quePosition = Math.floor(Math.random() * 4);
-  let cuandoPosition = Math.floor(Math.random() * 5);
-
-  return (
-    who[quienPosition] + " " + what[quePosition] + " " + when[cuandoPosition]
-  );
+let getRandomNum = () => {
+  let num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"];
+  let randomNum = Math.floor(Math.random() * num.length);
+  return num[randomNum];
 };
 
-console.log(generateExcuse());
+let getRandomSuit = () => {
+  let suit = ["&spades;", "&clubs;", "&diams;", "&hearts;"];
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  return suit[randomSuit];
+};
 
-//Generador de Excusas
+let getRandomColor = () => {
+  let color = ["#ff0000", "#000000"];
+  let randomColor = Math.floor(Math.random() * color.length);
+  return color[randomColor];
+};
